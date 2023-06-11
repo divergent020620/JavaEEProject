fdb1d32006c61776751290ef7ccfae1abb105f7b
Van_XQ <van_xq@outlook.com>
2023-06-11 18:45:35 +0800

    A3 final

--- vueTran/config-eureka-client/src/main/java/com/example/config/configApplication.java
+++ vueTran/config-eureka-client/src/main/java/com/example/config/configApplication.java
@@ -1,15 +1,18 @@
-package com.example.api;
+package com.example.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableConfigServer
 @EnableDiscoveryClient
-public class ApiApplication {
+
+public class configApplication {
     public static void main(String[] args) {
-        SpringApplication.run(ApiApplication.class, args);
+        SpringApplication.run(configApplication.class, args);
     }
 
 }
